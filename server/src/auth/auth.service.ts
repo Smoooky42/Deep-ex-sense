56c013bf2161a7b03d2b1aea4b74a1e4fa05ceca
Smoooky42 <Fazekash42@yandex.ru>
2025-01-23 21:56:20 +0300

    tokens

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,12 +1,21 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common'
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
+import { Response } from 'express'
+
 import { UserService } from '../user/user.service'
 import { PrismaService } from '../prisma.service'
 import { AuthDto } from './dto/auth.dto'
 
 @Injectable()
 export class AuthService {
-	constructor(private jwt: JwtService, private userService: UserService, private prisma: PrismaService) {}
+	EXPIRE_DAY_REFRESH_TOKEN = 1
+	REFRESH_TOKEN_NAME = 'refreshToken'
+
+	constructor(private jwt: JwtService,
+				private userService: UserService,
+				private prisma: PrismaService,
+				private configSevise: ConfigService) {}
 
 	async login(dto: AuthDto) {
 		const user = await this.validateUser(dto)
@@ -20,6 +29,16 @@ export class AuthService {
 		if (oldUser) throw new BadRequestException('User already exists')
 
 		const user = await this.userService.create(dto)
+		const tokens = this.issueTokens(user.id)
+
+		return {user, ...tokens}
+	}
+
+	async getNewTokens(refreshToken: string) {
+		const result = await this.jwt.verifyAsync(refreshToken)
+		if (!result) throw new UnauthorizedException('Невалидный refresh токен')
+
+		const user = await this.userService.getById(result.id)
 		const tokens = this.issueTokens(user.id)
 
 		return {user, ...tokens}
@@ -44,5 +63,45 @@ export class AuthService {
 		if (!user) throw new NotFoundException(`User ${dto.email} not found`)
 
 		return user
+	}
+
+	async validateOAuthLogin(req: any) {
+		let user = await this.userService.getByEmail(req.user.email)
+
+		if (!user) {
+			user = await this.prisma.user.create({
+				data: {
+					email: req.user.email,
+					password: req.user.password,
+					picture: req.user.picture,
+				}
+			})
+		}
+		const tokens = this.issueTokens(user.id)
+
+		return {user, ...tokens}
+	}
+
+	addRefreshTokenToResponse(res: Response, refreshToken: string) {
+		const expiresIn = new Date()
+		expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+
+		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+			httpOnly: true,
+			domain: this.configSevise.get('SERVER_DOMAIN'),
+			expires: expiresIn,
+			secure: true,
+			sameSite: 'none'
+		})
+	}
+
+	removeRefreshTokenFromResponse(res: Response) {
+		res.cookie(this.REFRESH_TOKEN_NAME,'', {
+			httpOnly: true,
+			domain: this.configSevise.get('SERVER_DOMAIN'),
+			expires: new Date(0),
+			secure: true,
+			sameSite: 'none'
+		})
 	}
 }
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,23 +1,9 @@
-import { Body, Post, Controller, HttpCode, UsePipes, ValidationPipe } from '@nestjs/common'
+import {Controller} from '@nestjs/common'
 
-import { AuthDto } from '../auth/dto/auth.dto'
-import { AuthService } from '../auth/auth.service'
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly authService: AuthService) {}
+  constructor() {}
 
-  @UsePipes(new ValidationPipe())
-  @HttpCode(200)
-  @Post('login')
-  async login(@Body() dto: AuthDto) {
-    return this.authService.login(dto)
-  }
 
-  @UsePipes(new ValidationPipe())
-  @HttpCode(200)
-  @Post('register')
-  async register(@Body() dto: AuthDto) {
-    return this.authService.register(dto)
-  }
 }
